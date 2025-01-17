5ea327fd41fb81a2fda2ad8bed5582d6f9a45755
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-01-16 21:59:33 -0800

    Initial Commit

---
+++ MyJourneyTests/MyJourneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyJourneyTests.swift
+//  MyJourneyTests
+//
+//  Created by Mitch Wintrow on 1/16/25.
+//
+
+import Testing
+@testable import MyJourney
+
+struct MyJourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
